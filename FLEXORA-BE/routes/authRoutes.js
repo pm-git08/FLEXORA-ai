b793b7befa3a80bf69f85d1ce0b93ffe16521403
Prajjwal <154116375+pm-git08@users.noreply.github.com>
2025-04-21 06:55:35 +0530

    Delete FLEXORA-BE directory

--- FLEXORA-BE/routes/authRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const { loginUser, registerUser } = require('../controllers/authController');
-
-const router = express.Router();
-
-router.post('/login', loginUser);
-router.post('/signup', registerUser);
-
-module.exports = router;
--- FLEXORA-BE/utils/generateToken.js
+++
@@ -1,7 +0,0 @@
-const jwt = require('jsonwebtoken');
-
-const generateToken = (id) => {
-    return jwt.sign({ id }, 'secretKey', { expiresIn: '3d' }); // Replace 'secretKey' later with env
-};
-
-module.exports = generateToken;
